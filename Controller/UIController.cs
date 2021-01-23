c11b8c5700a6858ec10f6525816617f2773f0e04
GU JIAWEI RP <19013309@myrp.edu.sg>
2021-01-23 14:03:23 +0800

    Add project files.

---
+++ Controller/UIController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace C200.Controllers
+{
+    public class UIController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult EmptyLots()
+        {
+            ViewData["dbtable"] = DBUtl.GetTable("SELECT * FROM LotAvailable ");
+            return View();
+        }
+
+        public IActionResult Distance()
+        {
+            return View();
+        }
+
+        public IActionResult Test()
+        {
+            return View();
+        }
+
+
+
+    }
+}
